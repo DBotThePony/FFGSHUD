87bae071899e2296633d954610baf1f1d96e4a32
DBotThePony <dbotthepony@yandex.ru>
2018-04-04 06:19:17 +0700

    oops

--- lua/ffgshud/wepselect.lua
+++ lua/ffgshud/wepselect.lua
@@ -297,10 +297,10 @@ local function WheelBind(self, ply, bind, pressed, weapons)
 	FFGSHUD.SelectWeaponPos = FFGSHUD.SelectWeaponPos + (bind == 'invnext' and 1 or -1)
 
 	if FFGSHUD.SelectWeaponPos < 1 then
-		for i = 1, 7 do
+		for i = 1, 6 do
 			slot = slot - 1
 
-			if slot < 0 then
+			if slot < 1 then
 				slot = 6
 			end
 
