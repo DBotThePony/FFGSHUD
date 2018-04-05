3e62aaec81d4063c049c0372a770576138dac778
DBotThePony <dbotthepony@yandex.ru>
2018-04-05 13:40:13 +0700

    Add rare gravity death message

--- lua/ffgshud/killfeed.lua
+++ lua/ffgshud/killfeed.lua
@@ -18,6 +18,8 @@ local language = language
 local Color = Color
 local RealTimeL = RealTimeL
 local table = table
+local math = math
+local Vector = Vector
 
 local notices = {}
 local function npcColor(npc)
@@ -43,6 +45,7 @@ function FFGSHUD:AddDeathNotice(attacker, attackerTeam, inflictor, victim, victi
 
 	weapon = weapon:upper()
 
+	local gravity = math.random() < 0.1
 	local entry = {}
 
 	entry.weaponColor = Color(255, 255, 255)
@@ -80,6 +83,19 @@ function FFGSHUD:AddDeathNotice(attacker, attackerTeam, inflictor, victim, victi
 		entry.victimColor = npcColor(victim)
 	end
 
+	entry.gravity = gravity
+
+	if gravity then
+		entry.velocityAttacker = {math.random(-100, 25) / 25, math.random(-15, 5)}
+		entry.velocityWeapon = {math.random(-25, 25) / 25, math.random(-25, 5)}
+		entry.velocityVictim = {math.random(-25, 100) / 25, math.random(-17, 5)}
+		entry.posAttacker = {0, 0}
+		entry.posWeapon = {0, 0}
+		entry.posVictim = {0, 0}
+
+		entry.ttl = RealTimeL() + displayTime * 2
+	end
+
 	table.insert(notices, entry)
 
 	return true
@@ -101,24 +117,32 @@ function FFGSHUD:DrawDeathNotice()
 		local x = x
 		local mult = 1
 		local cut = false
+		local gravity = entry.gravity
 
 		if entry.fadeInEnd > time then
 			cut = true
 			mult = time:progression(entry.fadeInStart, entry.fadeInEnd)
 			render.PushScissorRect(0, y, x, y + H * mult)
 		elseif entry.fadeOutStart < time then
-			cut = true
 			mult = 1 - time:progression(entry.fadeOutStart, entry.fadeOutEnd)
-			render.PushScissorRect(0, y, x, y + H * mult)
+
+			if not gravity then
+				cut = true
+				render.PushScissorRect(0, y, x, y + H * mult)
+			end
 		end
 
-		local w, h = self:DrawShadowedTextAligned(self.KillfeedFont, entry.victim, x, y, entry.victimColor)
+		local addx, addy = gravity and entry.posVictim[1] or 0, gravity and entry.posVictim[2] or 0
+		local w, h = self:DrawShadowedTextAligned(self.KillfeedFont, entry.victim, x + addx, y + addy, entry.victimColor)
 		x = x - w - ScreenSize(5)
-		w, h = self:DrawShadowedTextAligned(self.KillfeedFont, entry.weapon, x, y, entry.weaponColor)
+
+		local addx, addy = gravity and entry.posWeapon[1] or 0, gravity and entry.posWeapon[2] or 0
+		w, h = self:DrawShadowedTextAligned(self.KillfeedFont, entry.weapon, x + addx, y + addy, entry.weaponColor)
 
 		if entry.validAttacker then
 			x = x - w - ScreenSize(5)
-			self:DrawShadowedTextAligned(self.KillfeedFont, entry.attacker, x, y, entry.attackerColor)
+			local addx, addy = gravity and entry.posAttacker[1] or 0, gravity and entry.posAttacker[2] or 0
+			self:DrawShadowedTextAligned(self.KillfeedFont, entry.attacker, x + addx, y + addy, entry.attackerColor)
 		end
 
 		y = y + (h + space) * mult
@@ -132,11 +156,33 @@ end
 function FFGSHUD:ThinkDeathNotice()
 	local toRemove
 	local time = RealTimeL()
+	local ftime = RealFrameTime() * 11
 
 	for i, entry in ipairs(notices) do
 		if entry.ttl < time then
 			toRemove = toRemove or {}
 			table.insert(toRemove, i)
+		elseif entry.gravity and entry.fadeOutStart < time then
+			local velx, vely = entry.velocityAttacker[1], entry.velocityAttacker[2]
+			velx = velx * 0.98
+			vely = vely + ftime
+			entry.posAttacker[1] = entry.posAttacker[1] + velx
+			entry.posAttacker[2] = entry.posAttacker[2] + vely
+			entry.velocityAttacker[1], entry.velocityAttacker[2] = velx, vely
+
+			velx, vely = entry.velocityWeapon[1], entry.velocityWeapon[2]
+			velx = velx * 0.98
+			vely = vely + ftime
+			entry.posWeapon[1] = entry.posWeapon[1] + velx
+			entry.posWeapon[2] = entry.posWeapon[2] + vely
+			entry.velocityWeapon[1], entry.velocityWeapon[2] = velx, vely
+
+			velx, vely = entry.velocityVictim[1], entry.velocityVictim[2]
+			velx = velx * 0.98
+			vely = vely + ftime
+			entry.posVictim[1] = entry.posVictim[1] + velx
+			entry.posVictim[2] = entry.posVictim[2] + vely
+			entry.velocityVictim[1], entry.velocityVictim[2] = velx, vely
 		end
 	end
 
